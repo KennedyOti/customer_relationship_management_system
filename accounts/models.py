d6fe609e06d5fc1bdfdae2a8d6153ab7ddbcd410
KennedyOti <otienokennedy972@gmail.com>
2024-04-04 11:46:30 +0300

    db creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Product Class
+class Products (models.Model):
+    product_name = models.CharField(max_length=200, null=True)
+    product_type = models.CharField(max_length=200, null=True)
+    product_qtty = models.IntegerField(null=True)
+    product_bp = models.FloatField(null=True)
+    product_sp = models.FloatField(null=True)
+    date_added = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.product_name
+    
+# Sales Class
+
+class Sale(models.Model):
+
+    #product=
+    date_made = models.DateTimeField(auto_now_add=True, null=True)
